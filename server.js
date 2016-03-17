f86554ce7fc2d64fa7817cc5fee5dcb228072730
Ronan Connolly <hi@ronanconnolly.ie>
2016-03-17 19:56:41 +0000

    Added my literature review on the advantages of js for fullstack dev

--- server.js
+++ server.js
@@ -7,6 +7,11 @@
 
     app.use(express.static(__dirname + '/dist/'));
 
+    // The advantages of using JavaScript for full stack development with an emphasis on Node.js (Ronan Connolly, 2015)
+    app.get('/js-advantages-in-fullstack-dev', function(req, res){
+        res.sendfile(__dirname + '/external-docs/js-advantages-in-fullstack-dev-ronanconnolly-2015.pdf');
+    });
+
     app.listen(port, function() {
         console.log('Our app is running on port: ' + port);
     });
