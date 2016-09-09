842ad0f6040dfc5241df67d54afd25e8870207e4
Ronan Connolly <ronan.connolly2@ie.ibm.com>
2016-09-09 23:04:23 +0100

    Added literature review to portfolio and a link to the fyp project review.

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 (function() {
-    "use strict";
+  "use strict";
 
-    var express = require('express');
-    var app = express();
-    var port = process.env.PORT || 8080;
+  var express = require('express');
+  var app = express();
+  var port = process.env.PORT || 8080;
 
-    app.use(express.static(__dirname + '/dist/'));
-    app.use(express.static(__dirname + '/external-docs/'));
+  app.use(express.static(__dirname + '/dist/'));
+  app.use(express.static(__dirname + '/external-docs/'));
+  app.use(express.static(__dirname + '/external-docs/GMIT-Catering/'));
+
+  // The advantages of using JavaScript for full stack development with an emphasis on Node.js (Ronan Connolly, 2015)
+  app.get('/js-advantages-in-fullstack-dev', function(req, res) {
+    res.sendfile(__dirname + '/external-docs/js-advantages-in-fullstack-dev-ronanconnolly-2015.pdf');
+  });
+  app.get('/js-lit-review', function(req, res) {
+    res.sendfile(__dirname + '/external-docs/js-advantages-in-fullstack-dev-ronanconnolly-2015.pdf');
+  });
+
+  // FYP Project Report (Dissertation) - Food Ordering System for the GMIT Catering Company (Ronan Connolly, Vladislav Marisevs 2016)
+  app.get('/fyp-project-report', function(req, res) {
+    res.sendfile(__dirname + '/external-docs/Food Ordering System for the GMIT Catering Company.pdf');
+  });
+
+  // fyp presentation
+  app.get('/fyp-pres', function(req, res) {
     app.use(express.static(__dirname + '/external-docs/GMIT-Catering/'));
+    res.sendfile(__dirname + '/external-docs/GMIT-Catering/index.html');
+  });
 
-    // The advantages of using JavaScript for full stack development with an emphasis on Node.js (Ronan Connolly, 2015)
-    app.get('/js-advantages-in-fullstack-dev', function(req, res) {
-        res.sendfile(__dirname + '/external-docs/js-advantages-in-fullstack-dev-ronanconnolly-2015.pdf');
-    });
-    app.get('/js-lit-review', function(req, res) {
-        res.sendfile(__dirname + '/external-docs/js-advantages-in-fullstack-dev-ronanconnolly-2015.pdf');
-    });
-
-    // fyp presentation
-    app.get('/fyp-pres', function(req, res) {
-        app.use(express.static(__dirname + '/external-docs/GMIT-Catering/'));
-        res.sendfile(__dirname + '/external-docs/GMIT-Catering/index.html');
-    });
-
-    app.listen(port, function() {
-        console.log('Our app is running on port: ' + port);
-    });
+  app.listen(port, function() {
+    console.log('Our app is running on port: ' + port);
+  });
 })();
